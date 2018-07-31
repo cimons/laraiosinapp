2f659b24ecf3bf8ddf93e07ce7e543566fbd3eb9
Simon Shrestha <cimon77@gmail.com>
2018-07-31 17:24:44 +0545

    Initial Commit

---
+++ src/LaraIosInAppProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cimons\LaraIosInApp;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraIosInAppProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
